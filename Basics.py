8e3ccd0f3cd25e775978d16767583209599fbe30
Sandeep M Waghmare <sandeepwaghmare1999@gmail.com>
2022-03-10 19:53:52 +0530

    Day1

---
+++ Basics.py
@@ -0,0 +1,26 @@
+# why we use python  ->
+# lst = [1,2,3,4]  keyword as your variable name
+# tuple case scenario
+# print return
+
+# new_var = (1,)
+# print(type(new_var))
+'''
+Github
+linkdin
+inftq
+'''
+def display():
+    return("Good morning..")
+
+print(display())
+
+
+# feasible solu, Optimal solu
+
+
+'''
+Github
+linkdin
+inftq
+'''
